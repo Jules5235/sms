b12a0b4742489702cc8b7feeba6fcb4df438a9fc
Ciprian Nicolae <48139709+cnecrea@users.noreply.github.com>
2024-02-10 05:42:26 +0200

    Update notify.py
    Eliminando CONF_RECIPIENT and added ATTR_TARGET instead.
    
    Removing: "recipient: +1XXXXXXXXXX" from configuration.yaml/notify.yaml.
    
    Now it's possible to specify the phone number within "targets".

--- custom_components/smsto/notify.py
+++ custom_components/smsto/notify.py
@@ -1,16 +1,18 @@
 """SMSto platform for notify component."""
-from http import HTTPStatus
 import json
 import logging
+from http import HTTPStatus
 
-from aiohttp.hdrs import CONTENT_TYPE
 import requests
 import voluptuous as vol
 
-from homeassistant.components.notify import PLATFORM_SCHEMA, BaseNotificationService
+from homeassistant.components.notify import (
+    ATTR_TARGET,
+    PLATFORM_SCHEMA,
+    BaseNotificationService,
+)
 from homeassistant.const import (
     CONF_API_KEY,
-    CONF_RECIPIENT,
     CONF_SENDER,
     CONTENT_TYPE_JSON,
 )
@@ -22,18 +24,11 @@ BASE_API_URL = "https://api.sms.to"
 DEFAULT_SENDER = "hass"
 TIMEOUT = 5
 
-PLATFORM_SCHEMA = vol.Schema(
-    vol.All(
-        PLATFORM_SCHEMA.extend(
-            {
-                vol.Required(CONF_API_KEY): cv.string,  # This will now hold the Bearer token
-                vol.Required(CONF_RECIPIENT, default=[]): vol.All(
-                    cv.ensure_list, [cv.string]
-                ),
-                vol.Optional(CONF_SENDER, default=DEFAULT_SENDER): cv.string,
-            }
-        )
-    )
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_API_KEY): cv.string,  # This will now hold the Bearer token
+        vol.Optional(CONF_SENDER, default=DEFAULT_SENDER): cv.string,
+    }
 )
 
 
@@ -48,14 +43,21 @@ class SMStoNotificationService(BaseNotificationService):
     def __init__(self, config):
         """Initialize the service."""
         self.bearer_token = config[CONF_API_KEY]  # Store the Bearer token
-        self.recipients = config[CONF_RECIPIENT]
         self.sender = config[CONF_SENDER]
 
     def send_message(self, message="", **kwargs):
         """Send a message to a user."""
-        ploads = {
+        targets = kwargs.get(ATTR_TARGET)
+
+        if not targets:
+            _LOGGER.error("Error: 'targets' field is empty.")
+            return
+
+        
+        
+        payload = {
             "message": message,
-            "to": ",".join(self.recipients),
+            "to": ",".join(targets),
             "bypass_optout": True,  # Assuming this is always desired
             "sender_id": self.sender,
             "callback_url": kwargs.get("callback_url", ""),  # Optional callback
@@ -64,21 +66,27 @@ class SMStoNotificationService(BaseNotificationService):
         api_url = f"{BASE_API_URL}/sms/send"
         headers = {
             "Authorization": f"Bearer {self.bearer_token}",  # Add Bearer token to headers
-            "Content-Type": "application/json",
+            "Content-Type": CONTENT_TYPE_JSON,
         }
 
         resp = requests.post(
             api_url,
             timeout=TIMEOUT,
             headers=headers,
-            data=json.dumps(ploads),
+            data=json.dumps(payload),
         )
+
         if resp.status_code == HTTPStatus.OK:
             return
 
-        obj = json.loads(resp.text)
-        response_msg = obj.get("message")
-        response_code = obj.get("errorCode")
+        try:
+            obj = resp.json()
+            response_msg = obj.get("message")
+            response_code = obj.get("errorCode")
+        except json.JSONDecodeError:
+            response_msg = "Invalid JSON response"
+            response_code = None
+
         _LOGGER.error(
-            "Error %s : %s (Code %s)", resp.status_code, response_msg, response_code
+            "Error %s: %s (Code %s)", resp.status_code, response_msg, response_code
         )
